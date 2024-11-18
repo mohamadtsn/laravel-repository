f667d640bc81fb8b367f6734bb8372579ba814a4
Mohamad Tsn <mohamad.borzoyi@gmail.com>
2024-11-19 01:17:12 +0330

    Refactor and add doc-block

--- src/Commands/RepositoryMakeCommand.php
+++ src/Commands/RepositoryMakeCommand.php
@@ -3,80 +3,60 @@
 namespace Mohamadtsn\Repository\Commands;
 
 use Illuminate\Console\GeneratorCommand;
-use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use InvalidArgumentException;
 use Symfony\Component\Console\Input\InputOption;
 
 class RepositoryMakeCommand extends GeneratorCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'make:repository {name} {--model=}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Create a new repository class';
-
-    /**
-     * The type of class being generated.
-     *
-     * @var string
-     */
     protected $type = 'Repository';
 
     public function handle()
     {
         if (!$this->option('model')) {
-            $this->error('The "model" param is required.');
-            $this->warn('example: --model=User');
-
+            $this->error('The "model" option is required.');
+            $this->warn('Example: --model=User');
             return false;
         }
         return parent::handle();
     }
 
-    /**
-     * @param $name
-     * @return string
-     * @throws FileNotFoundException
-     */
-    protected function buildClass($name)
+    protected function buildClass($name): string
     {
         $stub = $this->files->get($this->getStub());
-
-        return $this->replaceNamespace($stub, $name)->replaceModel($stub)->replaceClass($stub, $name);
+        return $this->replaceNamespace($stub, $name)
+            ->replaceModel($stub)
+            ->replaceClass($stub, $name);
     }
 
-    /**
-     * @return string
-     */
     protected function getStub(): string
     {
-        return str_replace(['/', '\\'], DIRECTORY_SEPARATOR, rtrim(__DIR__, '/') . '/stubs/repository.stub');
+        return $this->resolveStubPath('/stubs/repository.stub');
     }
 
-    protected function replaceModel(&$stub)
+    protected function resolveStubPath($stub): string
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__ . $stub;
+    }
+
+    protected function replaceModel(&$stub): static
     {
         $modelClass = $this->parseModel($this->option('model'));
 
-        $stub = str_replace([
-            '{{ model }}',
-            '{{ namespacedModel }}',
-        ], [
-            class_basename($modelClass),
-            $modelClass,
-        ], $stub);
+        $replace = [
+            '{{ model }}' => class_basename($modelClass),
+            '{{ namespacedModel }}' => $modelClass,
+        ];
+
+        $stub = str_replace(array_keys($replace), array_values($replace), $stub);
 
         return $this;
     }
 
-    protected function parseModel($model)
+    protected function parseModel($model): string
     {
         if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
             throw new InvalidArgumentException('Model name contains invalid characters.');
@@ -85,15 +65,15 @@ class RepositoryMakeCommand extends GeneratorCommand
         return $this->qualifyModel($model);
     }
 
-    protected function getDefaultNamespace($rootNamespace)
+    protected function getDefaultNamespace($rootNamespace): string
     {
         return $rootNamespace . '\Repositories';
     }
 
-    protected function getOptions()
+    protected function getOptions(): array
     {
         return [
-            ['model=', 'm=', InputOption::VALUE_REQUIRED, 'Set model in new repository'],
+            ['model', 'm', InputOption::VALUE_REQUIRED, 'The model that the repository will handle'],
         ];
     }
 }
